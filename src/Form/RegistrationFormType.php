246f55f7b8eafedaa9b15146cadf5f69c940c70e
aris alexia <arisalexia3@gmail.com>
2023-02-14 13:52:18 +0100

    connexion/inscription/profile

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participants;
+use App\Entity\Sites;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use function Sodium\add;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo')
+            ->add('prenom', TextType::class, ["label" => "Prénom"])
+            ->add('nom')
+            ->add('telephone')
+            ->add('mail', TextType::class, ["label" => "Email"])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'options' => ['attr' => ['class' => 'password-field']],
+                'attr' => ['autocomplete' => 'new-password'],
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('sitesNoSite', EntityType::class,
+                [
+                    "class" => Sites::class,
+                    "choice_label" => "nom_site",
+                    "label" => "Ville de rattachement"
+                ])
+            #->add('imageFile', FileType::class,
+               # [
+               #     "label" => "Ma photo"
+               # ])
+            ->add('Enregistrer', SubmitType::class)
+            ->add('Annuler', ResetType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participants::class,
+        ]);
+    }
+}
