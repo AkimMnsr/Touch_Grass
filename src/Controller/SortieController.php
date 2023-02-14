371e11d96cbc13bc58ee338b874a5fb7abeadf0d
AkimMnsr <akim.mnasri@gmail.com>
2023-02-14 14:17:53 +0100

    itération am/0.2 commit affichage sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Inscriptions;
+use App\Entity\Sorties;
+use App\Repository\InscriptionsRepository;
+use App\Repository\SortiesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SortieController extends AbstractController
 {
-    #[Route('/sortie', name: 'sortie_sortie')]
-    public function sortie(): Response
+    #[Route('/sortie/{id}', name: 'sortie_sortie')]
+    public function sortie(
+        int $id,
+        SortiesRepository $sortiesRepository,
+        InscriptionsRepository $inscriptionsRepository
+    ): Response
     {
-        return $this->render('sortie/sortie.html.twig');
+        $sortie = $sortiesRepository->findOneBy(["id" =>$id]);
+        return $this->render('sortie/sortie.html.twig',
+            compact('sortie')
+        );
     }
 }
