9348507d051d327db3d6ea2451b69377c5ba8bc1
AkimMnsr <akim.mnasri@gmail.com>
2023-02-15 17:06:27 +0100

    commit am/addSortie [Adding sortie creation & some css]

---
+++ src/Form/LieuxType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieux;
+use App\Entity\Villes;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\DomCrawler\Field\ChoiceFormField;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Choice;
+
+class LieuxType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom_lieu', EntityType::class,
+            [
+              'class' => Lieux::class,
+              'choice_label' => 'NomLieu',
+            ])
+            ->add('latitude')
+            ->add('longitude')
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Lieux::class,
+        ]);
+    }
+}
