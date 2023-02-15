9189085f5e2d4da6844de93b20a141bf946a1982
AkimMnsr <akim.mnasri@gmail.com>
2023-02-15 17:05:29 +0100

    commit am/addSortie [Adding sortie creation & some css]

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -3,10 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Inscriptions;
+use App\Entity\Lieux;
 use App\Entity\Sorties;
+use App\Form\AddSortieType;
+use App\Repository\EtatsRepository;
 use App\Repository\InscriptionsRepository;
+use App\Repository\LieuxRepository;
 use App\Repository\SortiesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -18,10 +25,46 @@ class SortieController extends AbstractController
     // Function Adding 'sortie'
     #[IsGranted('ROLE_USER')]
     #[Route('/addSortie', name: 'addSortie')]
-    public function addSortie() : Response
+    public function addSortie(
+        EntityManagerInterface $em,
+        EtatsRepository $etatsRepository,
+        Request $request
+    ) : Response
     {
 
-        return $this->render('sortie/addSortie.html.twig');
+
+        $sortie = new Sorties();
+        $sortieForm = $this->createForm(AddSortieType::class,$sortie);
+        $sortieForm->handleRequest($request);
+        $organisateur = $this->getUser();
+        $sortie->setOrganisateur($organisateur);
+
+        if ($sortieForm->isSubmitted()) {
+            try {
+
+               if( $sortieForm->get("save")->isClicked()) {
+                   $save = $etatsRepository->findOneBy(['id' => 1]);
+                   $sortie->setEtatsNoEtat($save);
+               } else {
+                   $publish = $etatsRepository->findOneBy(['id' => 2]);
+                   $sortie->setEtatsNoEtat($publish);
+               };
+
+
+                if ($sortieForm->isValid()) {
+                    $em->persist($sortie);
+                    $em->flush();
+                    $idSortie = $sortie->getId();
+                    return $this->redirectToRoute('sortie_sortie', array('id' =>$idSortie));
+                }
+
+            } catch (Exception $exception) {
+                dd($exception->getMessage());
+            }
+        }
+        return $this->render('sortie/addSortie.html.twig',[
+            'formSortie' => $sortieForm->createView()
+        ]);
 
     }
 
