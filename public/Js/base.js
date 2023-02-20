978f1e55fe7f71b3e0bae889c7ab99f8493e2efc
AkimMnsr <akim.mnasri@gmail.com>
2023-02-20 09:33:28 +0100

    push from AKim [safe add]

--- public/Js/base.js
+++ public/Js/base.js
@@ -1,6 +1,19 @@
 
-//Delete infobulle
-function deleteme() {
-    let x = document.getElementById('art-info');
-    x.style.display = "none";
+
+
+
+window.onload = function() {
+   if (localStorage.getItem("popupShown") === null) {
+   document.getElementById("art-info").style.display = "block";
+   document.getElementById("close-popup").addEventListener("click", function() {
+   document.getElementById("art-info").style.display = "none";
+   localStorage.setItem("popupShown", true);
+});
+} else {
+      document.getElementById("art-info").style.display = "none";
+   }
+};
+
+function logout() {
+   localStorage.removeItem('popupShown');
 }
