36d2ab0705b055965a1863e85d5dc62b72d5e0a9
AkimMnsr <akim.mnasri@gmail.com>
2023-02-21 10:10:51 +0100

    push from AKim [addin regle metier & correctiff ]

--- src/Entity/Lieux.php
+++ src/Entity/Lieux.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\LieuxRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: LieuxRepository::class)]
 class Lieux
@@ -14,15 +15,43 @@ class Lieux
     private ?int $id = null;
 
     #[ORM\Column(length: 30)]
+    #[Assert\NotBlank(
+        message: 'Un nom de lieu doit être renseigné'
+    )]
+    #[Assert\Regex(
+        pattern: '/^([^0-9]*)$/',
+        match: true,
+        message: 'Le nom de la sortie ne peux contenir de nombre',
+    )]
     private ?string $nom_lieu = null;
 
-    #[ORM\Column(length: 30, nullable: true)]
+    #[ORM\Column(length: 30, nullable: false)]
+    #[Assert\NotBlank(
+        message: 'Une rue doit être renseignée'
+    )]
+    #[Assert\Length(
+        min: 10,
+        max: 30,
+        minMessage: "Le nom ne peut être inférieur a {{ min }} caractères.",
+        maxMessage: "Le pseudo ne peut pas être supérieur à {{ max }} caractères."
+    )]
+
     private ?string $rue = null;
 
     #[ORM\Column(nullable: true)]
+    #[Assert\Regex(
+        pattern: '^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$',
+        match: true,
+        message: 'Cela ne ressemble pas à une latitude',
+    )]
     private ?float $latitude = null;
 
     #[ORM\Column(nullable: true)]
+    #[Assert\Regex(
+        pattern: '^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$',
+        match: true,
+        message: 'Cela ne ressemble pas à une longitude',
+    )]
     private ?float $longitude = null;
 
     #[ORM\ManyToOne]
