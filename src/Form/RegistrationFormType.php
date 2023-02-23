ac8ea26a56ef4c1cc8971a84055dab946fe8a655
aris alexia <arisalexia3@gmail.com>
2023-02-23 15:28:26 +0100

    tqt

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,8 +6,6 @@ use App\Entity\Participants;
 use App\Entity\Sites;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
@@ -15,11 +13,9 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Vich\UploaderBundle\Form\Type\VichFileType;
-use function Sodium\add;
 
 class RegistrationFormType extends AbstractType
 {
